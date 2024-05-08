71292d0724fa2b2e2b9b38021bc16c08a5687b31
vy34 <nguyenthingocvy332004@gmail.com>
2024-05-08 16:44:42 +0700

    Log out và Sửa giao diện

--- app/src/main/java/com/example/wavesoffood/Fragment/ProfileFragment.kt
+++ app/src/main/java/com/example/wavesoffood/Fragment/ProfileFragment.kt
@@ -1,11 +1,13 @@
 package com.example.wavesoffood.Fragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import com.example.wavesoffood.LoginActivity
 import com.example.wavesoffood.R
 import com.example.wavesoffood.databinding.FragmentProfileBinding
 import com.example.wavesoffood.model.UserModel
@@ -52,6 +54,13 @@ class ProfileFragment : Fragment() {
             val address=binding.address.text.toString()
 
             updatedUserData(name,email,phone,address)
+        }
+
+        binding.logOutButton.setOnClickListener {
+            val intent=Intent(requireContext(),LoginActivity::class.java)
+            auth.signOut()
+            startActivity(intent)
+            requireActivity().finish()
         }
 
         return binding.root
